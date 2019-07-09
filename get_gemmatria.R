4f42a98cd9593c89170fd4ace435c35f7af5d285
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2019-07-09 13:53:10 -0400

    Update get_gemmatria.R

--- get_gemmatria.R
+++ get_gemmatria.R
@@ -87,5 +87,85 @@ get_gemmatria_rt<-function(text){
   splt<-unlist(strsplit(text,split = " "))
   
   splt<-unname(sapply(splt,function(x) strsplit(x,split="")))
-  splt
+  splt<-sapply(splt,function(x) x[1])
+  
+  gem<-sapply(splt, function(x)plyr::revalue(x,c( "א"=1,
+                                                  "ב"=2,
+                                                  "ג"=3,
+                                                  "ד"=4,
+                                                  "ה"=5,
+                                                  "ו"=6,
+                                                  "ז"=7,
+                                                  "ח"=8,
+                                                  "ט"=9,
+                                                  "י"=10,
+                                                  "כ"=20,
+                                                  "ך"=20,
+                                                  "ל"=30,
+                                                  "מ"=40,
+                                                  "ם"=40,
+                                                  "נ"=50,
+                                                  "ן"=50,
+                                                  "ס"=60,
+                                                  "ע"=70,
+                                                  "פ"=80,
+                                                  "ף"=80,
+                                                  "צ"=90,
+                                                  "ץ"=90,
+                                                  "ק"=100,
+                                                  "ר"=200,
+                                                  "ש"=300,
+                                                  "ת"=400),
+                                             warn_missing = FALSE ))
+  
+  gem<-unname(gem)
+  gem<-as.numeric(gem)
+  
+  sum(gem)
+  
+  
+}
+
+
+get_gemmatria_st<-function(text){
+  splt<-unlist(strsplit(text,split = " "))
+  
+  splt<-unname(sapply(splt,function(x) strsplit(x,split="")))
+  splt<-sapply(splt,function(x) x[length(x)])
+  
+  gem<-sapply(splt, function(x)plyr::revalue(x,c( "א"=1,
+                                                  "ב"=2,
+                                                  "ג"=3,
+                                                  "ד"=4,
+                                                  "ה"=5,
+                                                  "ו"=6,
+                                                  "ז"=7,
+                                                  "ח"=8,
+                                                  "ט"=9,
+                                                  "י"=10,
+                                                  "כ"=20,
+                                                  "ך"=20,
+                                                  "ל"=30,
+                                                  "מ"=40,
+                                                  "ם"=40,
+                                                  "נ"=50,
+                                                  "ן"=50,
+                                                  "ס"=60,
+                                                  "ע"=70,
+                                                  "פ"=80,
+                                                  "ף"=80,
+                                                  "צ"=90,
+                                                  "ץ"=90,
+                                                  "ק"=100,
+                                                  "ר"=200,
+                                                  "ש"=300,
+                                                  "ת"=400),
+                                             warn_missing = FALSE ))
+  
+  gem<-unname(gem)
+  gem<-as.numeric(gem)
+  
+  sum(gem)
+  
+  
 }
